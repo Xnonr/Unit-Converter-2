84f74a4723b576172f17088fbbea22fb274f2e4a
Maxence Brette <maxbrette@gmail.com>
2018-06-15 17:46:36 -0500

    Initial Commit

---
+++ Unit Converter 2Tests/Unit_Converter_2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Unit_Converter_2Tests.swift
+//  Unit Converter 2Tests
+//
+//  Created by Maxence Brette on 6/15/18.
+//  Copyright © 2018 Maxence Brette. All rights reserved.
+//
+
+import XCTest
+@testable import Unit_Converter_2
+
+class Unit_Converter_2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
